4ef87489e0ece1fb9080448c137e23611b5d4df0
caiomunhoz <caio.gmunhoz@gmail.com>
2025-12-15 20:06:10 -0300

    feat: add entry

--- src/web_automation/pages/wallet.py
+++ src/web_automation/pages/wallet.py
@@ -6,3 +6,28 @@ class WalletPage(BasePage):
 
     def open_entry_form(self):
         self.click(self.ADD_ENTRIES_BUTTON)
+
+    def add_entry(self, entry):
+        return self.driver.execute_script(
+            '''
+            const payload = arguments[0]
+            
+            return fetch ("/wallet/api/proxy/wallet-app/entry/store", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(payload)
+            })
+            .then(async response => {
+                return {
+                    status: response.status,
+                    ok: response.ok,
+                    body: await response.text()
+                };
+            })
+            .catch(error => {
+                return {
+                    status: -1,
+                    error: error.toString()
+                };
+            });
+            ''', entry)
